fb55766bd9fe92a461645213f310d8534b1626be
huitzilin <techstemonline@gmail.com>
2022-03-02 08:18:13 -0600

    chore: add environment variables for Maps API

--- environment.ts
+++ environment.ts
@@ -7,11 +7,17 @@ const envConfig = {
   development: {
     aws_s3: {
       region: process.env.AWS_S3_REGION
+    },
+    google: {
+      maps_api_key: process.env.GOOGLE_MAPS_API_KEY
     }
   },
   production: {
     aws_s3: {
       region: process.env.AWS_S3_REGION
+    },
+    google: {
+      maps_api_key: process.env.GOOGLE_MAPS_API_KEY
     }
   }
 }[env];
