cd05e1f310031455e3e2a1844334fb75ef9144cc
huitzilin <techstemonline@gmail.com>
2022-03-02 08:49:30 -0600

    feat: reverse-geocode route
    Converts geographic coordinates into a human-readable address

--- src/routes/maps.ts
+++ src/routes/maps.ts
@@ -24,4 +24,18 @@ app.get('/geocode', async (req, res) => {
   res.json(response.results);
 });
 
+app.get('/reverse-geocode', async (req, res) => {
+  let { lat, lng } = req.query;
+  if (!lat || !lng) return res.status(400).json('Please provide valid coordinates');
+
+  let response: GeocodingResponse;
+  try {
+    let { data } = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${env.google.maps_api_key}`);
+    response = data;
+  } catch(error) {
+    return res.json(error);
+  }
+  res.json(response.results);
+});
+
 export default app;
