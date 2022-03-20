b4436f8560f1f288e9db902470a1bb4f16753228
anastasiotomas@gmail.com <anastasiotomas@gmail.com>
2022-03-19 23:54:23 -0300

    feat(errorMiddlewares)ready

--- src/config/generateToken.ts
+++ src/config/generateToken.ts
@@ -2,7 +2,7 @@
 import jwt from 'jsonwebtoken';
 
 const generateToken = (id: any) => {
-    return jwt.sign({id}, `${process.env.JWT_SECRET}`, {expiresIn: '7d'}); // process.env.JWT_SECRET is the secret key that we set in the .env file
+    return jwt.sign({id}, `${process.env.JWT_SECRET}`, {expiresIn: '7d'});
 }
 
 export default generateToken;
